587e631a00243635add104140b06150201814a56
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2014-02-04 00:21:21 +0100

    Splited into Agent and Manager

---
+++ manager/src/main/java/org/picketbox/jsmpolicy/test/Manager.java
@@ -0,0 +1,14 @@
+package org.picketbox.jsmpolicy.test;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+class Manager {
+	public static void main(String[] args){
+		
+		Client client = Client.create(); // REST client
+		
+		System.out.println("ok");
+	}
+}
