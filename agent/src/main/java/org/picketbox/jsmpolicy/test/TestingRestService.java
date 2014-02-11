1377fe2d77dc5a70d93cb21df44a2306f4ee8b6e
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2014-02-11 18:09:57 +0100

    dependency - jboss-deployment-structure.xml

--- agent/src/main/java/org/picketbox/jsmpolicy/test/TestingRestService.java
+++ agent/src/main/java/org/picketbox/jsmpolicy/test/TestingRestService.java
@@ -5,10 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import java.io.File;
+import java.io.StringWriter;
 import java.lang.SecurityManager;
+import java.security.AccessControlException;
 import java.security.Policy;
 
-// http://localhost:8080/RESTfulExample/rest/testing/
+// http://localhost:8080/JsmPolicyTestingAgent/rest/testing/securitymanager
 
 @Path("/testing")
 public class TestingRestService {
@@ -16,19 +19,43 @@ public class TestingRestService {
 	@GET
 	@Path("/securitymanager")
 	public String getSecurityManager() {
-		return System.getSecurityManager().getClass().getName();
+		SecurityManager sm = System.getSecurityManager();
+		return sm==null ? "null" : sm.getClass().getName();
 	}
 	
 	@GET
 	@Path("/policy")
 	public String getPolicy() {
-		return Policy.getPolicy().getClass().getName();
+		Policy policy = Policy.getPolicy(); 
+		return policy==null ? "null" : policy.getClass().getName();
 	}
 	
 	@GET
 	@Path("/policyfile")
 	public String getPolicyFile() {
 		return System.getProperty("java.security.policy");
+	}
+	
+	@GET
+	@Path("/test")
+	public String getTest() {
+		StringWriter sw = new StringWriter();
+		sw.write("test10: /etc/passwd:\t");
+		try{
+			File f = new File("/etc/passwd");
+			if(f.canWrite()) sw.write("WRITABLE");
+			else if(f.canRead()) sw.write("READABLE");
+			else sw.write("INACCESSIBLE");
+		}
+		catch(AccessControlException e){
+			sw.write("DENIED");
+		}
+		catch(Exception e){
+			sw.write("Exception "+e.toString());
+		}
+		sw.write("\n");
+		
+		return sw.toString();
 	}
 	
 	@GET
